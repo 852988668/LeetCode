4a5ad02bf210f1a33d0fb5fe88458e108c5031b5
852988668 <852988668@qq.com>
2018-05-09 10:42:16 +0800

    更新

--- app/src/main/java/com/hzt/leetcode/MainActivity.java
+++ app/src/main/java/com/hzt/leetcode/MainActivity.java
@@ -4,17 +4,27 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.hzt.leetcode.question.IQuestion;
+import com.hzt.leetcode.question.easy.Que007;
 import com.hzt.leetcode.question.medium.Que006;
+
+import butterknife.OnClick;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
+    IQuestion question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        IQuestion question = new Que006();
+        question = new Que007();
         question.run();
     }
 
+    @OnClick(R.id.btn_run)
+    void runQue() {
+        question.run();
+    }
 }
